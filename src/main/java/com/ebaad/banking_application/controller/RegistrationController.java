34957a228a7ed13441fc5d5a17e2eac9892cf3b3
ebaadazam <ebaad.azam@memorly.ai>
2025-03-23 18:00:22 +0530

    updated branch

---
+++ src/main/java/com/ebaad/banking_application/controller/RegistrationController.java
@@ -0,0 +1,26 @@
+package com.ebaad.banking_application.controller;
+
+import com.ebaad.banking_application.entity.User;
+import com.ebaad.banking_application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User registered successfully!";
+    }
+}
